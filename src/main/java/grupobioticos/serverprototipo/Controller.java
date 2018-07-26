85fd1f72eb19aee0c4b9ccde5cf72901b65cf0d9
Francisco <francisco.serrano372@gmail.com>
2018-07-26 16:56:02 -0300

    Se implementó un controlador básico que permite comunicación

--- src/main/java/grupobioticos/serverprototipo/Controller.java
+++ src/main/java/grupobioticos/serverprototipo/Controller.java
@@ -1,15 +1,41 @@
 package grupobioticos.serverprototipo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
 
-    @GetMapping
-    public String hello() {
-        return "que onda bigote";
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/plain")
+    public String prueba() {
+        return "respuesta de prueba de la api";
     }
+
+    @RequestMapping(value = "/lecturainformacion", method = RequestMethod.GET, produces = "text/plain")
+    public String lecturaInformacion() {
+        System.out.println("Llegó request de LECTURA INFORMACION");
+        return "lectura de información";
+    }
+
+    @RequestMapping(value = "/busquedacoincidencias", method = RequestMethod.GET, produces = "text/plain")
+    public String busquedaCoincidencias() {
+        System.out.println("Llegó request de BUSQUEDA COINCIDENCIAS");
+        return "búsqueda de coincidencias";
+    }
+
+    @RequestMapping(value = "/construccionensambles", method = RequestMethod.GET, produces = "text/plain")
+    public String construccionEnsambles() {
+        System.out.println("Llegó request de CONSTRUCCION ENSAMBLES");
+        return "construcción de ensambles";
+    }
+
+    @RequestMapping(value = "/generacioninformes", method = RequestMethod.GET, produces = "text/plain")
+    public String generacionInformes() {
+        System.out.println("Llegó request de GENERACION INFORMES");
+        return "generacion de informes";
+    }
+
 }
