9c436b660aa215d1083ac801c3bb38d3808558c8
Francisco <francisco.serrano372@gmail.com>
2018-08-01 12:34:00 -0300

    Se ejecutan los jar en caso de correr de forma local

--- src/main/java/grupobioticos/serverprototipo/Controller.java
+++ src/main/java/grupobioticos/serverprototipo/Controller.java
@@ -1,22 +1,23 @@
 package grupobioticos.serverprototipo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = {
         "http://localhost:4200",
         "https://client-prototipo.herokuapp.com",
-        "http://client-prototipo.herokuapp.com/"
+        "http://client-prototipo.herokuapp.com/",
+        "client-prototipo.herokuapp.com/"
 })
 public class Controller {
 
     private CommandsConfiguration configuration;
-    private int port;
+    private boolean runningLocal;
 
     @Autowired
     public void setConfiguration(CommandsConfiguration configuration) {
@@ -25,7 +26,8 @@ public class Controller {
 
     @Autowired
     public void setPort(Environment environment) {
-        this.port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("server.port")));
+        int port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("server.port")));
+        this.runningLocal = port == 8080;
     }
 
     @GetMapping(value = "/")
@@ -36,47 +38,49 @@ public class Controller {
     @GetMapping(value = "/lecturainformacion")
     public String lecturaInformacion() {
         System.out.println("Llegó request de LECTURA INFORMACION");
-        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_1());
 
-        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
+        if (runningLocal)
+            runCommand(this.configuration.getSubgrupo_1());
 
-        return String.format("lectura de información %s", modoEjecucion);
+        return String.format("lectura de información - runningLocal: %s", this.runningLocal);
     }
 
     @GetMapping(value = "/busquedacoincidencias")
     public String busquedaCoincidencias() {
         System.out.println("Llegó request de BUSQUEDA COINCIDENCIAS");
-        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_2());
 
-        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
+        if (runningLocal)
+            runCommand(this.configuration.getSubgrupo_2());
 
-        return String.format("búsqueda de coincidencias %s", modoEjecucion);
+        return String.format("búsqueda de coincidencias - runningLocal: %s", this.runningLocal);
     }
 
     @GetMapping(value = "/construccionensambles")
     public String construccionEnsambles() {
         System.out.println("Llegó request de CONSTRUCCION ENSAMBLES");
-        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_3());
 
-        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
+        if (runningLocal)
+            runCommand(this.configuration.getSubgrupo_3());
 
-        return String.format("construcción de ensambles %s", modoEjecucion);
+        return String.format("construcción de ensambles - runningLocal: %s", this.runningLocal);
     }
 
     @GetMapping(value = "/generacioninformes")
     public String generacionInformes() {
         System.out.println("Llegó request de GENERACION INFORMES");
-        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_4());
 
-        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
+        if (runningLocal)
+            runCommand(this.configuration.getSubgrupo_4());
 
-        return String.format("generacion de informes %s", modoEjecucion);
+        return String.format("generacion de informes - runningLocal: %s", this.runningLocal);
     }
 
-    private void introducirRetardo(int cantidadSegundos) {
+    private void runCommand(String command) {
+        System.out.println("COMANDO A EJECUTAR: " + command);
+
         try {
-            Thread.sleep(cantidadSegundos * 1000);
-        } catch (InterruptedException e) {
+            Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
