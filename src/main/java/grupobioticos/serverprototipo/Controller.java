d6a438b8d65c938a161ebe6068e240bf6cab248c
Francisco <francisco.serrano372@gmail.com>
2018-08-01 12:18:48 -0300

    Se detecta ejecución local o remota

--- src/main/java/grupobioticos/serverprototipo/Controller.java
+++ src/main/java/grupobioticos/serverprototipo/Controller.java
@@ -1,70 +1,76 @@
 package grupobioticos.serverprototipo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200", "https://client-prototipo.herokuapp.com"})
+@CrossOrigin(origins = {
+        "http://localhost:4200",
+        "https://client-prototipo.herokuapp.com",
+        "http://client-prototipo.herokuapp.com/"
+})
 public class Controller {
 
     private CommandsConfiguration configuration;
-
-    @Autowired
-    Environment environment;
+    private int port;
 
     @Autowired
     public void setConfiguration(CommandsConfiguration configuration) {
         this.configuration = configuration;
     }
 
+    @Autowired
+    public void setPort(Environment environment) {
+        this.port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("server.port")));
+    }
+
     @GetMapping(value = "/")
     public String prueba() {
-        System.out.println("PORT: " + environment.getProperty("local.server.port"));
-
         return "respuesta de prueba de la api";
     }
 
     @GetMapping(value = "/lecturainformacion")
     public String lecturaInformacion() {
         System.out.println("Llegó request de LECTURA INFORMACION");
-
-//        int port = Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port")));
-
         System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_1());
 
-//        introducirRetardo(1);
+        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
 
-        return "lectura de información";
+        return String.format("lectura de información %s", modoEjecucion);
     }
 
     @GetMapping(value = "/busquedacoincidencias")
     public String busquedaCoincidencias() {
         System.out.println("Llegó request de BUSQUEDA COINCIDENCIAS");
+        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_2());
 
-        introducirRetardo(1);
+        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
 
-        return "búsqueda de coincidencias";
+        return String.format("búsqueda de coincidencias %s", modoEjecucion);
     }
 
     @GetMapping(value = "/construccionensambles")
     public String construccionEnsambles() {
         System.out.println("Llegó request de CONSTRUCCION ENSAMBLES");
+        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_3());
 
-        introducirRetardo(1);
+        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
 
-        return "construcción de ensambles";
+        return String.format("construcción de ensambles %s", modoEjecucion);
     }
 
     @GetMapping(value = "/generacioninformes")
     public String generacionInformes() {
         System.out.println("Llegó request de GENERACION INFORMES");
+        System.out.println("COMANDO A EJECUTAR: " + this.configuration.getSubgrupo_4());
 
-        introducirRetardo(1);
+        String modoEjecucion = this.port == 8080 ? "LOCAL" : "REMOTE";
 
-        return "generacion de informes";
+        return String.format("generacion de informes %s", modoEjecucion);
     }
 
     private void introducirRetardo(int cantidadSegundos) {
